988161b4208c3f82fa6a01bbeb419443c5a53142
taea <igiari@gmail.com>
2013-07-29 21:32:48 +0900

    Require rack

--- config.rb
+++ config.rb
@@ -83,3 +83,14 @@ set :haml, { :ugly => true, :format => :html5 }
 
 # Middleman LiveReload
 activate :livereload
+
+# Heroku Deploy 
+require 'rack'
+require 'rack/contrib/try_static'
+
+use ::Rack::TryStatic, 
+  :root => "build",    	# where middleman files are generated
+  :urls => %w[/]        	# match all requests
+  :try => ['.html', 'index.html', '/index.html'] # try these postfixes sequentially
+# 404
+run lambda { [404, {'Content-Type' => 'text/plain'}, [' File not found.']]}
