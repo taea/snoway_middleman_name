06611ff08d81a5637b44db623fe38e903b70bed7
taea <igiari@gmail.com>
2013-10-25 18:28:50 +0900

    rm unnecessary gemfile settings

--- config.rb
+++ config.rb
@@ -115,15 +115,15 @@ module Rack
   end
 end
 
-use Rack::Deflater
-use Rack::TryStatic, :root => "build", :urls => %w[/], :try => ['.html', 'index.html', '/index.html']
+# use Rack::Deflater
+# use Rack::TryStatic, :root => "build", :urls => %w[/], :try => ['.html', 'index.html', '/index.html']
 
 # Run your own Rack app here or use this one to serve 404 messages:
-run lambda{ |env|
-  not_found_page = File.expand_path("../build/404.html", __FILE__)
-  if File.exist?(not_found_page)
-    [ 404, { 'Content-Type'  => 'text/html'}, [File.read(not_found_page)] ]
-  else
-    [ 404, { 'Content-Type'  => 'text/html' }, ['404 - page not found'] ]
-  end
-}
+#run lambda{ |env|
+#  not_found_page = File.expand_path("../build/404.html", __FILE__)
+#  if File.exist?(not_found_page)
+#    [ 404, { 'Content-Type'  => 'text/html'}, [File.read(not_found_page)] ]
+#  else
+#    [ 404, { 'Content-Type'  => 'text/html' }, ['404 - page not found'] ]
+#  end
+#}
